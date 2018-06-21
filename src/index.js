512f6f55cfed2f4e0975c0173bf6bb4a244c8410
guilima <gui.lima30@gmail.com>
2018-06-20 23:32:50 -0300

    refactor(index, modules): Change react-router-redux import by react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,34 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore } from 'redux'
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import createHistory from 'history/createBrowserHistory'
-import { Route } from 'react-router'
-import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux';
-const BRAND = process.env.BRAND;
 
 import Routes from './routes';
 
-// Create a history of your choosing (we're using a browser history in this case)
-const history = createHistory()
-// Build the middleware for intercepting and dispatching navigation actions
-const middleware = routerMiddleware(history)
 const root = document.createElement('div');
 
 document.body.appendChild(root);
-// Add the reducer to your store on the `routing` key
-const store = createStore(
-    combineReducers({
-        router: routerReducer
-    }),
-    applyMiddleware(middleware)
-);
+
+const store = createStore(() => ({strong: 1})
+, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render((
     <Provider store={store}>
-        { /* Tell the Router to use our enhanced history */ }
-        <ConnectedRouter history={history}>
+        <BrowserRouter>
             <Routes />
-        </ConnectedRouter>
+        </BrowserRouter>
     </Provider>
 ), root);
